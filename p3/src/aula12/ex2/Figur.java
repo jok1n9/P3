ac160c3dbee309cf546b0b2274ff60e7bd73772c
Joaquim Andrade <joaquimandrade@ua.pt>
2021-01-11 21:17:38 +0000

    Update Figur.java
    perimetro

--- p3/src/aula12/ex2/Figur.java
+++ p3/src/aula12/ex2/Figur.java
@@ -1,6 +1,7 @@
 package aula12.ex2;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 import aula5.ex1.Circle;
@@ -12,12 +13,13 @@ public class Figur {
 	public static void main(String[] args) {
 		
 		ArrayList<Figure> a= new ArrayList<Figure>();
-		a.add(new Circle(3));
+		a.add(new Circle(2));
 		a.add(new Circle(1, 3, 2));
 		a.add(new Square(2));
 		a.add(new Square(3, 4, 2));
 		a.add(new Rectangle(2, 3));
 		System.out.println("Maior Figura \n" + maior(a)+ "\n");
+		System.out.println("Maior Perimetro \n" + maiorperimetro(a)+ "\n");
 		System.out.println("Area total \n" + area(a)+ "\n");
 		System.out.println("Area total dos quadrados\n" + area(a, "Square") + "\n");
 	}
@@ -25,7 +27,10 @@ public class Figur {
 	{
 		return list.stream().sorted((f1, f2) -> f1.compareTo(f2)).collect(Collectors.toList()).get(list.size() - 1);
 	}
-	
+	private static Figure maiorperimetro(ArrayList<Figure> list)
+	{
+		return list.stream().sorted(Comparator.comparingDouble(Figure::getPerimeter) ).collect(Collectors.toList()).get(list.size() - 1);
+	}
 	private static double area(ArrayList<Figure> list)
 	{
 		return list.stream().mapToDouble(Figure::getArea).sum();
